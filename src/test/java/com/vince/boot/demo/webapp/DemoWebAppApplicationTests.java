62a372cc5ae435e937826b4ba1fbf23283fe6fe7
vincenzolucchese <vincenzolucchese@gmail.com>
2017-10-03 18:04:27 +0200

    Update DemoWebAppApplicationTests.java

--- src/test/java/com/vince/boot/demo/webapp/DemoWebAppApplicationTests.java
+++ src/test/java/com/vince/boot/demo/webapp/DemoWebAppApplicationTests.java
@@ -1,6 +1,7 @@
 package com.vince.boot.demo.webapp;
 
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
