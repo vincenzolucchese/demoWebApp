b5a44e358c0073d2b54e40612df6bdb3a794daf6
Vincenzo Lucchese <v.lucchese@tdnet.it>
2017-10-03 17:56:58 +0200

    ok

---
+++ src/main/java/com/vince/boot/demo/webapp/DemoWebAppApplication.java
@@ -0,0 +1,12 @@
+package com.vince.boot.demo.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebAppApplication.class, args);
+	}
+}
